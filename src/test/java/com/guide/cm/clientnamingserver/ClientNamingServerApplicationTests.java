31cb406b2884f8321eeab30196f195f8d1810e82
prateek4b <shivcharanag293@gmail.com>
2021-06-27 09:23:46 +0530

    commit

---
+++ src/test/java/com/guide/cm/clientnamingserver/ClientNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide.cm.clientnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
